7b860e1593cad6251a868e5ee2b638248266707b
AQ <aku.peltonen@gmail.com>
2011-04-08 00:15:55 +0300

    Removed the tracking of deleted files

--- tsoha/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tsoha::Application.config.secret_token = '8a5c01da1233805b0e15da9d7cf4779f3b02431c9cf09a22594384c4963b99526ed3d49e22235d9506fa583e8739d0df29073784b2df4d9d09929e47e1705daa'
