192796c51eda50fcf2a64793f970fc65f16081e0
AQ <aku.peltonen@gmail.com>
2011-09-13 22:56:10 +0300

    Added validation for uniqueness of a user being added as a project member

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,6 @@ class Member < ActiveRecord::Base
   belongs_to :member_role
   belongs_to :project
   belongs_to :user
+  
+  validates_uniqueness_of :project_id, :scope => [:user_id], :message => "You cannot add the same user twice!"
 end
